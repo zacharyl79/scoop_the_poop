d463a7a39d02dc5381a9fde460feff19b79ea37f
1N4 <sample@bths.edu>
2025-04-11 08:32:12 -0400

    Initial Commit

---
+++ scoop_the_poop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scoop_the_poop
+//
+//  Created by Student on 4/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scoop_the_poop/scoop_the_poopApp.swift
@@ -0,0 +1,17 @@
+//
+//  scoop_the_poopApp.swift
+//  scoop_the_poop
+//
+//  Created by Student on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct scoop_the_poopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
