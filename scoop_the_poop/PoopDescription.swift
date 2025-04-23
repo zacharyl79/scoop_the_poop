098a6512b7a22f43d5c23e8b7f06a2434b01da53
bths <bths@edu>
2025-04-23 16:51:47 -0400

    Remove marker animation

--- scoop_the_poop/PoopDescription.swift
+++ scoop_the_poop/PoopDescription.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct PoopDescription: View {
+    @EnvironmentObject private var handler: SQLiteHandler
+    @Binding var resolvedPoopID: [Int32: CGFloat]
     var poopInfo: PoopMarker
 
     var body: some View {
@@ -23,7 +26,14 @@ struct PoopDescription: View {
             }
             
             Button("Remove marker") {
-                
+                withAnimation(.easeOut(duration: 3)) {
+                    resolvedPoopID[poopInfo.id] = CGFloat(100)
+                }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) { // Wait for the animation to finish
+                handler.resolveMarker(unique_identifier: poopInfo.id) // Remove marker logic
+                handler.fetchNonResolvedMarkers() // Refresh the data
+            }
             }
             .buttonStyle(BorderedProminentButtonStyle())
             .padding()
